1074df66f3a44023264bc791fc4d2d476abd97c2
HACKERMD <markusdherrmann@gmail.com>
2015-06-22 18:46:26 +0200

    Refactor datafusion package

--- tmt/datafusion/__init__.py
+++ tmt/datafusion/__init__.py
@@ -3,6 +3,15 @@ from tmt.util import load_config
 
 version = '0.1.0'
 
+
+logo = '''
+     _      _         __         _          
+  __| |__ _| |_ __ _ / _|_  _ __(_)___ _ _         datafusion (%(version)s)
+ / _` / _` |  _/ _` |  _| || (_-< / _ \ ' \        Fuse Jterator data into one HDF5 file.
+ \__,_\__,_|\__\__,_|_|  \_,_/__/_\___/_||_|       https://github.com/HackerMD/TissueMAPSToolbox
+
+'''
+
 # Create configuration dictionary that defines default parameters
 config_filename = join(dirname(realpath(__file__)), 'datafusion.config')
 
