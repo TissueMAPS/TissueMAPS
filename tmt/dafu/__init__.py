25bd53939294f3b02cf76953aae658bc78d096aa
HACKERMD <markusdherrmann@gmail.com>
2015-06-25 03:28:17 +0200

    Rename datafusion package to dafu

---
+++ tmt/dafu/__init__.py
@@ -0,0 +1,18 @@
+from os.path import join, dirname, realpath
+from tmt.util import load_config
+
+version = '0.1.0'
+
+
+logo = u'''
+     _       __
+  __| |__ _ / _|_  _         dafu (%(version)s)
+ / _` / _` |  _| || |        DAta FUsion for Jterator projects.
+ \__,_\__,_|_|  \_,_|        https://github.com/HackerMD/TissueMAPSToolbox
+
+'''
+
+# Create configuration dictionary that defines default parameters
+config_filename = join(dirname(realpath(__file__)), 'dafu.config')
+
+config = load_config(config_filename)
