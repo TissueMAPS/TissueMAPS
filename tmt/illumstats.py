f25c1a0c641e0b20380c1ceb5960f50c994e289f
HACKERMD <markusdherrmann@gmail.com>
2015-06-25 03:30:48 +0200

    Move illumination correction from illuminati to illumstats module

--- tmt/illumstats.py
+++ tmt/illumstats.py
@@ -1,62 +1,145 @@
 import h5py
 import re
 import numpy as np
+import scipy.ndimage as ndi
+from tmt.imageutil import np_array_to_vips_image
 from tmt.util import regex_from_format_string
 
 
-class Illumstats:
+def illum_correction_vips(orig_image, mean_image, std_image):
+    '''
+    Correct fluorescence microscopy image for illumination artifacts
+    using the image processing library Vips.
+
+    Parameters
+    ----------
+    orig_image: Vips.Image[Vips.BandFormat.USHORT]
+        image that should be corrected
+    mean_image: Vips.Image[Vips.BandFormat.DOUBLE]
+        matrix of mean values (same dimensions as `orig_image`)
+    std_image: Vips.Image[Vips.BandFormat.DOUBLE]
+        matrix of standard deviation values (same dimensions as `orig_image`)
+
+    Returns
+    -------
+    Vips.Image[Vips.BandFormat.USHORT]
+        corrected image
+    '''
+    # If we don't cast the conditional image, the result of ifthenelse
+    # will be UCHAR
+    orig_format = orig_image.get_format()
+    cond = (orig_image == 0).cast(orig_format)
+    img = cond.ifthenelse(1, orig_image)
+
+    # Do all computations with double precision
+    img = img.cast('double')
+    img = img.log10()
+    img = (img - mean_image) / std_image
+    img = img * std_image.avg() + mean_image.avg()
+    img = 10 ** img
+
+    # Cast back to UINT16 or whatever the original image was
+    img = img.cast(orig_format)
+
+    return img
+
+
+def illum_correction(orig_image, mean_mat, std_mat, fix_pixels=False):
+    """
+    Correct fluorescence microscopy image for illumination artifacts.
+
+    Parameters
+    ----------
+    orig_image: numpy.ndarray[numpy.uint16]
+        image that should be corrected
+    mean_mat: numpy.ndarray[numpy.float64]
+        matrix of mean values (same dimensions as `orig_image`)
+    std_mat: numpy.ndarray[numpy.float64]
+        matrix of standard deviation values (same dimensions as `orig_image`)
+    
+    Returns
+    -------
+    numpy.ndarray[numpy.uint16]
+        corrected image
+    """
+    # correct intensity image for illumination artifact
+    img = orig_image.copy()
+    img = img.astype(np.float64)
+    img[img == 0] = 1
+    img = (np.log10(img) - mean_mat) / std_mat
+    img = (img * np.mean(std_mat)) + np.mean(mean_mat)
+    img = 10 ** img
+
+    if fix_pixels:
+        # fix "bad" pixels with non numeric values (NaN or Inf)
+        ix_bad = np.logical_not(np.isfinite(img))
+        if ix_bad.sum() > 0:
+            med_filt_image = ndi.filters.median_filter(img, 3)
+            img[ix_bad] = med_filt_image[ix_bad]
+            img[ix_bad] = med_filt_image[ix_bad]
+
+        # fix extreme pixels
+        percent = 99.9999
+        thresh = np.percentile(img, percent)
+        img[img > thresh] = thresh
+
+    return img.astype(np.uint16)
+
+
+class Illumstats(object):
     '''
     Utility class for illumination correction statistics.
 
     It provides the mean and standard deviation images and the channel number.
     The statistics were calculated for each pixel position over all image sites
-    acquired in the same channel.
+    acquired in the same channel [1]_.
 
-    Reference
-    ---------
-    Battich et al. 2015 Methods
+    References
+    ----------
+    .. [1] Stoeger T, Battich N, Herrmann MD, Yakimovich Y, Pelkmans L. 2015.
+           Computer vision for image-based transcriptomics. Methods.
     '''
 
-    def __init__(self, filename, cfg):
+    def __init__(self, filename, cfg, matlab=False):
         '''
-        Initiate Illumstats class.
+        Initialize Illumstats class.
+
+        Images are either `Vips` or `numpy`,
+        depending on `USE_VIPS_LIBRARY` configuration.
 
         Parameters
         ----------
         filename: str
-                  path to the statistics file
+            path to the statistics file
         cfg: Dict[str, str]
-             configuration settings
+            configuration settings
+        matlab: bool, optional
+            if statistics were calculated with Matlab
         '''
         self.cfg = cfg
         self.filename = filename
+        self.matlab = matlab
+        self.use_vips = self.cfg['USE_VIPS_LIBRARY']
         self._statistics = None
         self._mean_image = None
         self._std_image = None
         self._channel = None
 
     @property
-    def statistics(self):
-        '''
-        Load precomputed statistics and return mean and std images.
-
-        By default the statistics files are HDF5 files
-        with the following structure:
-        /stat_values            Group
-        /stat_values/mean       Dataset
-        /stat_values/std        Dataset
-
-        Returns
-        -------
-        Tuple[numpy.ndarray]
-        '''
+    def _statistics(self):
         if not self._statistics:
             stats = h5py.File(self.filename, 'r')
             stats = stats['stat_values']
-            # Matlab transposes arrays when saving them to HDF5 files
-            # so we have to transpose them back!
-            mean_image = np.array(stats['mean'][()], dtype='float64').conj().T
-            std_image = np.array(stats['std'][()], dtype='float64').conj().T
+            mean_image = np.array(stats['mean'][()], dtype='float64')
+            std_image = np.array(stats['std'][()], dtype='float64')
+            if self.matlab:
+                # Matlab transposes arrays when saving them to HDF5 files
+                # so we have to transpose them back!
+                mean_image = mean_image.conj().T
+                std_image = std_image.conj().T
+            if self.use_vips:
+                mean_image = np_array_to_vips_image(mean_image)
+                std_image = np_array_to_vips_image(std_image)
             self._statistics = (mean_image, std_image)
         return self._statistics
 
@@ -66,7 +149,7 @@ class Illumstats:
         Returns
         -------
         int
-        channel number
+            channel number
         '''
         if not self._channel:
             regexp = regex_from_format_string(self.cfg['STATS_FILE_FORMAT'])
@@ -83,9 +166,9 @@ class Illumstats:
         '''
         Returns
         -------
-        numpy.ndarray
-        image matrix of mean values at each pixel position
-        (statistic pre-calculated at each pixel position over all image sites)
+        numpy.ndarray[numpy.float64] or Vips.Image[Vips.BandFormat.DOUBLE]
+            image matrix of mean values at each pixel position
+            (statistic calculated at each pixel position over all image sites)
         '''
         if not self._mean_image:
             self._mean_image = self._statistics[0]
@@ -96,10 +179,31 @@ class Illumstats:
         '''
         Returns
         -------
-        numpy.ndarray
-        image matrix of standard deviation values
-        (statistic pre-calculated at each pixel position over all image sites)
+        numpy.ndarray[numpy.float64] or Vips.Image[Vips.BandFormat.DOUBLE]
+            image matrix of standard deviation values
+            (statistic calculated at each pixel position over all image sites)
         '''
         if not self._std_image:
             self._std_image = self._statistics[1]
         return self._std_image
+
+    def correct_illumination(self, image):
+        '''
+        Correct image for illumination artifacts.
+
+        Parameters
+        ----------
+        image: numpy.ndarray or Vips.Image
+            image that should be corrected
+
+        Returns
+        -------
+        numpy.ndarray[numpy.unit16] or Vips.Image[Vips.BandFormat.USHORT]
+            corrected image
+        '''
+        if self.use_vips:
+            img = illum_correction_vips(image, self.mean_image, self.std_image)
+        else:
+            img = illum_correction(image, self.mean_image, self.std_image)
+
+        return img
