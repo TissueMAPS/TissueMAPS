ba99421b36583d7aaeb01dc150e731cc3faecf1b
hafenr <robin.hafen@gmail.com>
2015-08-21 13:38:29 +0200

    Fix bug where decoding of hash didn't work

--- tmaps/extensions/encrypt.py
+++ tmaps/extensions/encrypt.py
@@ -9,7 +9,7 @@ SALT_ALPHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 def decode(pk):
     hasher = Hashids(salt=current_app.config['HASHIDS_SALT'],
                      min_length=8)
-    return hasher.decode(pk)
+    return hasher.decode(pk)[0]
 
 
 def encode(pk):
