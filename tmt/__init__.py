da249ab6d7c344f7b3e9d90feecb73587c6b16df
HACKERMD <markusdherrmann@gmail.com>
2015-09-07 18:30:53 +0200

    Update __init__

--- tmt/__init__.py
+++ tmt/__init__.py
@@ -2,6 +2,5 @@ from os.path import join, dirname, realpath
 import utils
 
 # Create configuration dictionary that defines default parameters
-config_filename = join(dirname(realpath(__file__)), 'tmt.config')
-config = utils.load_config(config_filename)
-# utils.check_config(config)
+cfg_filename = join(dirname(realpath(__file__)), 'tmt.cfg')
+cfg = utils.read_yaml(cfg_filename)
