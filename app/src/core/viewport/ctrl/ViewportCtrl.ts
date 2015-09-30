4dd2410f74d2905ad65dc5798d508d42abd74a4c
hafenr <robin.hafen@gmail.com>
2015-09-30 15:20:35 +0200

    Rename directories of UI code to be more descriptive

--- app/src/core/viewport/ctrl/ViewportCtrl.ts
+++ app/src/core/viewport/ctrl/ViewportCtrl.ts
@@ -1,21 +1,13 @@
 /*
  * This controller will be instantiated when the viewport is
  * created. Don't use this directly via ng-controller.
+ * The viewport argument is injected by Viewport when it is attached to
+ * the document.
  */
 angular.module('tmaps.core').controller(
     'ViewportCtrl',
         ['$scope', 'viewport', 'authService', 'appstateService',
             function($scope, viewport, authService, appstateService) {
-
-    $scope.viewport = viewport;
-
-    this.isUserAuthenticated = function() {
-        return authService.isAuthenticated();
-    };
-
-    this.isUserViewingSnapshot = function() {
-        return appstateService.hasCurrentState() && appstateService.getCurrentState().isSnapshot;
-    };
 
 }]);
 
