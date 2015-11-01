ddafc78c1a588e4108cbe2a545a290f97c43cb3e
HACKERMD <markusdherrmann@gmail.com>
2015-11-01 14:03:11 +0100

    Fix bugs in jenkins scipts

--- scripts/test_all_packages.sh
+++ scripts/test_all_packages.sh
@@ -5,9 +5,6 @@
 # The script expects to be run from the TOP-LEVEL folder of tmlibrary
 #
 
-# exit at the first error
-set -e
-
 # Checks if we are in the top level folder
 if [[ ! -d src ]] || [[ ! -d docs ]] || [[ ! -d scripts ]]; then
     echo "Please execute script from the top-level folder of tmlibrary"
