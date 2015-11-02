6a9a8c303f1f1ba925f18479da588cb9fb1635df
HACKERMD <markusdherrmann@gmail.com>
2015-11-02 16:20:54 +0100

    Remove logging from unittest

--- src/tmlib/tests/unit_tests/test_dataset_read_write.py
+++ src/tmlib/tests/unit_tests/test_dataset_read_write.py
@@ -1,13 +1,10 @@
 import os
-import logging
 import random
 import tempfile
 import numpy as np
 import unittest
 from tmlib.readers import DatasetReader
 from tmlib.writers import DatasetWriter
-
-logger = logging.getLogger(__name__)
 
 
 class TestDatasetReaderWriter(unittest.TestCase):
@@ -31,13 +28,11 @@ class TestDatasetReaderWriter(unittest.TestCase):
          for i in xrange(len(actual))]
 
     def test_atomic_dataset(self):
-        logger.info('test ATOMIC dataset')
         arr_write = np.array(np.random.random((1000, 1500)))
         arr_read = self.write_array_to_HDF5_file_and_read_it_back(arr_write)
         self.assertEqualNumpy(arr_write, arr_read)
 
     def test_compound_dataset(self):
-        logger.info('test COMPOUND dataset')
         arr_write = list()
         for i in xrange(100):
             arr_write.append(np.empty((random.randint(100, 10000))))
