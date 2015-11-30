dbf5a7aead8deebe0c0e6cbacb46bab2e346f198
hafenr <robin.hafen@gmail.com>
2015-11-30 20:15:45 +0100

    Convert mapobject id to number and add document string

--- app/src/core/mapobject/MapObject.ts
+++ app/src/core/mapobject/MapObject.ts
@@ -1,7 +1,18 @@
-type MapObjectId = string;
+type MapObjectId = number;
 type MapObjectType = string;
 
+/**
+ * Objects that can be clicked and that are associated with 
+ * server-side information that can be used in various machine
+ * learning / data visualization procedures.
+ *
+ * The property `type` should correspond to the object type
+ * descriptor that is used on the server. An example for such a type
+ * could be 'cell' or 'nucleus' etc.
+ *
+ * The id attribute should identify a object uniquely within its type.
+ */
 interface MapObject extends Visualizable {
-    id: string;
+    id: MapObjectId;
     type: MapObjectType;
 }
