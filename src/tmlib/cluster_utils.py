b19dc88ed92dd3903d22ab7eb138205ed7e88cbb
HACKERMD <markusdherrmann@gmail.com>
2016-01-31 20:14:27 +0100

    Modify cluster workflow log messages

--- src/tmlib/cluster_utils.py
+++ src/tmlib/cluster_utils.py
@@ -1,4 +1,10 @@
 import gc3libs
+from .tmaps.workflow import Workflow
+from .tmaps.workflow import WorkflowStage
+from .tmaps.workflow import WorkflowStep
+from .tmaps.description import WorkflowDescription
+from .tmaps.description import WorkflowStageDescription
+from .tmaps.description import WorkflowStepDescription
 
 
 def format_stats_data(stats):
@@ -27,17 +33,81 @@ def format_stats_data(stats):
     return data
 
 
-def get_task_data(task):
+def get_task_info(task):
+    is_live_states = {
+        gc3libs.Run.State.SUBMITTED,
+        gc3libs.Run.State.RUNNING,
+        gc3libs.Run.State.STOPPED
+    }
+    is_done = task.execution.state == gc3libs.Run.State.TERMINATED
+    failed = task.execution.exitcode != 0
+    return {
+        'id': str(task),
+        'name': task.jobname,
+        'state': task.execution.state,
+        'is_live': task.execution.state in is_live_states,
+        'is_done': is_done,
+        'failed': is_done and failed,
+        'percent_done': 0.0  # TODO
+    }
+
+
+# def get_workflow_status(task, description):
+#     if not isinstance(task, Workflow):
+#         raise TypeError()
+#     if not isinstance(description, WorkflowDescription):
+#         raise TypeError()
+
+
+# def get_stage_status(task, description):
+#     if not isinstance(task, WorkflowStage):
+#         raise TypeError()
+#     if not isinstance(description, WorkflowStageDescription):
+#         raise TypeError()
+#     if task.jobname not in description.name:
+#         info = {
+#             'id': None,
+#             'name': description.name,
+#             'state': 'WAITING',
+#             'is_live': False,
+#             'is_done': False,
+#             'failed': False,
+#             'percent_done': 0.0
+#         }
+#     else:
+#         info = get_task_info(task)
+
+
+# def get_step_status(task, description):
+#     if not isinstance(task, WorkflowStep):
+#         raise TypeError()
+#     if not isinstance(description, WorkflowStepDescription):
+#         raise TypeError()
+#     if task.jobname not in description.name:
+#         info = {
+#             'id': None,
+#             'name': description.name,
+#             'state': 'WAITING',
+#             'is_live': False,
+#             'is_done': False,
+#             'failed': False,
+#             'percent_done': 0.0
+#         }
+#     else:
+#         info = get_task_info(task)
+
+
+def get_task_data(task, description=None):
     '''
     Provide the following data for each task and recursively for each
-    subtask (until `monitoring_depth` is reached) in form of a mapping:
+    subtask in form of a mapping:
 
         * "name" (*str*): name of task
         * "state" (*g3clibs.Run.State*): state of the task
         * "is_live" (*bool*): whether the task is currently processed
         * "is_done" (*bool*): whether the task is done
         * "failed" (*bool*): whether the task failed, i.e. terminated
-          unsuccessfully
+          with non-zero exitcode
         * "percent_done" (*float*): percent of subtasks that are done
 
     Parameters
@@ -48,8 +118,9 @@ def get_task_data(task):
     Returns
     -------
     dict
-        information for each task and its subtasks
+        information about each task and its subtasks
     '''
+    # TODO: Implement WAITING state for stages/steps that are not yet created
     def get_info(task_, i):
         is_live_states = {
             gc3libs.Run.State.SUBMITTED,
@@ -94,3 +165,45 @@ def get_task_data(task):
         return data
 
     return get_info(task, 0)
+
+
+def log_task_status(task_data, logger, monitoring_depth):
+    '''
+    Log the status of a submitted GC3Pie task.
+
+    Parameters
+    ----------
+    task_data: dict
+        information about each task and its subtasks
+    logger: logging.Logger
+        configured logger instance
+    monitoring_depth: int
+        recursion depth for subtask querying
+    '''
+    def log_recursive(data, i):
+        logger.info('%s: %s (%.2f %%)',
+                    data['name'], data['state'],
+                    data['percent_done'])
+        if i < monitoring_depth:
+            for subtd in data.get('subtasks', list()):
+                log_recursive(subtd, i+1)
+    log_recursive(task_data, 0)
+
+
+def log_task_failure(task_data, logger):
+    '''
+    Log the failure of a submitted GC3Pie task.
+
+    Parameters
+    ----------
+    task_data: dict
+        information about each task and its subtasks
+    logger: logging.Logger
+        configured logger instance
+    '''
+    def log_recursive(data, i):
+        if data['failed']:
+            logger.error('%s: failed', data['name'])
+        for subtd in data.get('subtasks', list()):
+            log_recursive(subtd, i+1)
+    log_recursive(task_data, 0)
