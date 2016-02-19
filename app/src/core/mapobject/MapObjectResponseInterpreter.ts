8b6bd9d544c558a9b7c50c0f7c7754511836c5a5
hafenr <robin.hafen@gmail.com>
2016-02-19 15:38:43 +0100

    Don't change outline coordinates client-side anymore

--- app/src/core/mapobject/MapObjectResponseInterpreter.ts
+++ app/src/core/mapobject/MapObjectResponseInterpreter.ts
@@ -15,20 +15,6 @@ class MapObjectResponseInterpreter {
         console.log(response);
         return _(response.ids).map((id) => {
             var coordinates = response.map_data.coordinates[id];
-
-            // TODO: The coordinates are given in i/j format by the server and are recalculated here
-            // to the x / -y format. This should be done on the server!
-            if (coordinates !== undefined) {
-                var ix;
-                var nCoords = coordinates.length;
-                for (ix = 0; ix < nCoords; ix++) {
-                    var i = coordinates[ix][0];
-                    var j = coordinates[ix][1];
-                    coordinates[ix][0] = j; // x
-                    coordinates[ix][1] = -i; // y
-                }
-            }
-
             var obj = new MapObject(id, type, 'polygon', {
                 coordinates: coordinates !== undefined ? coordinates : []
             });
