7c8c6d95a2e985fab6416dcb7ee662740c6a235c
hafenr <robin.hafen@gmail.com>
2016-02-26 19:08:42 +0100

    Fix uWSGI start script

--- scripts/run_wsgi.sh
+++ scripts/run_wsgi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Needs uwsgi! (installable via $ sudo pip install uwsgi)
-cd server && uwsgi --http :8080 --wsgi-file wsgi.py --master --processes 4 --threads 2 --stats 127.0.0.1:8081
+uwsgi --http :5002 --wsgi wsgi:app --master --processes 2 --threads 5 --stats 127.0.0.1:5003 -H ~/.virtualenvs/tmaps
 
