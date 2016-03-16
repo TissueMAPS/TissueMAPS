cc96586d7d3fdfcc8f31515fc122b7d0117d1765
hafenr <robin.hafen@gmail.com>
2016-03-16 14:28:11 +0100

    Faster hashing

--- tmaps/extensions/encrypt.py
+++ tmaps/extensions/encrypt.py
@@ -1,4 +1,5 @@
 from hashids import Hashids
+import base64
 from flask import current_app
 
 
@@ -9,18 +10,21 @@ class DecodeFailedException(Exception):
 SALT_ALPHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
 
-def decode(pk):
-    hasher = Hashids(salt=current_app.config['HASHIDS_SALT'],
-                     min_length=8)
-    decoded_id = hasher.decode(pk)
-    if not decoded_id:
-        raise DecodeFailedException(
-            'Was not able to decode the given string')
-    else:
-        return decoded_id[0]
+def decode(pk_str):
+    # hasher = Hashids(salt=current_app.config['HASHIDS_SALT'],
+    #                  min_length=8)
+    # decoded_id = hasher.decode(pk)
+    # if not decoded_id:
+    #     raise DecodeFailedException(
+    #         'Was not able to decode the given string')
+    # else:
+    #     return decoded_id[0]
+    # Convert from unicode to standard str
+    return int(base64.urlsafe_b64decode(str(pk_str))[5:])
 
 
 def encode(pk):
-    hasher = Hashids(salt=current_app.config['HASHIDS_SALT'],
-                     min_length=8)
-    return hasher.encode(pk)
+    # hasher = Hashids(salt=current_app.config['HASHIDS_SALT'],
+    #                  min_length=8)
+    # return hasher.encode(pk)
+    return base64.urlsafe_b64encode('tmaps' + str(pk))
