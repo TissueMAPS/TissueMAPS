14e5ab49c0f804925f4f0b133133ce796b9cc717
hafenr <robin.hafen@gmail.com>
2016-04-01 15:27:14 +0200

    Convert experiment fixture to yield fixture

--- src/tmlib/testing/fixtures/experiment.py
+++ src/tmlib/testing/fixtures/experiment.py
@@ -14,30 +14,28 @@ def _create_testexperiment(session, request, config, user,
         plate_acquisition_mode=plate_acquisition_mode,
         plate_format=plate_format
     )
+
     session.add(exp)
     session.commit()
 
-    def teardown():
-        pass
-        # session.delete(exp)
-        # session.commit()
-
-    request.addfinalizer(teardown)
     return exp
 
 
-@pytest.fixture(scope='function')
-def testexps(Session, request, config, roborobin, tmpdir_factory):
+@pytest.yield_fixture(scope='function')
+def testexps(session, request, config, roborobin, tmpdir_factory):
 
-    session = Session()
-
+    # Declare all test experiments.
     cellvoyager_384_1plate_2acquisitions_multiplexing = _create_testexperiment(
         session=session, request=request,
         plate_acquisition_mode='multiplexing', config=config,
-        name='Some experiment', user=roborobin,
+        name='Cellvoyager_384', user=roborobin,
         microscope_type='visiview', plate_format=96)
 
-    return {
+    # Yield them in a dict so they can be easily accessed by the consumer of
+    # the fixture.
+    test_experiments = {
         'cellvoyager_384_1plate_2acquisitions_multiplexing':
             cellvoyager_384_1plate_2acquisitions_multiplexing
     }
+
+    yield test_experiments
