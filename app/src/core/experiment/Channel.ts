0bf6711f132c4f417902628335a905b937d1540d
hafenr <robin.hafen@gmail.com>
2016-04-12 10:58:55 +0200

    Document Channel class

--- app/src/core/experiment/Channel.ts
+++ app/src/core/experiment/Channel.ts
@@ -16,31 +16,72 @@ class Channel implements Layer {
     private _currentZplane = 0;
     private _visible: boolean;
 
+    /**
+     * Constructor a new Channel.
+     *
+     * @class Channel
+     * @classdesc A channel represents a collection of layers acquired at
+     * different z levels. A channel is visualized as a single layer that can
+     * be colorized. 
+     * @param {ChannelArgs} args - An argument object of type ChannelArgs.
+     * @param {string} args.id - The id of this channel that was given by the server.
+     * @param {string} args.name - A descriptive name for this channel (displayed in the UI).
+     * @param {Array.<ChannelLayer>} args.name - A descriptive name for this channel (displayed in the UI).
+     * @param {boolean} args.visible - If the channel should be visible when it is added. Default is false.
+     */
     constructor(args: ChannelArgs) {
+        /**
+         * @property {string} name - The name of this channel.
+         * @default Color.WHITE
+         */
         this.name = args.name;
+        /**
+         * @property {string} id - The id of this channel that was given by the server.
+         * @default Color.WHITE
+         */
         this.id = args.id;
+
         args.layers.forEach((l) => {
             this._layers[l.zplane] = new ChannelLayer(l);
         });
         this.visible = args.visible;
     }
 
+    /**
+     * The layers that belong to this channel
+     * @name Channel#layers
+     * @type Array.<ChannelLayer>
+     * @default []
+     */
     get layers() {
         return _.values(this._layers);
     }
 
+    /**
+     * Add the channel to a map in order for it to be visualized.
+     * @param {ol.Map} map - An openlayers map object.
+     */
     addToMap(map: ol.Map) {
         _.values(this._layers).forEach((l) => {
             l.addToMap(map);
         });
     }
 
+    /**
+     * Remove the channel from the map making it no longer visible.
+     * @param {ol.Map} map - An openlayers map object.
+     */
     removeFromMap(map: ol.Map) {
         _.values(this._layers).forEach((l) => {
             l.removeFromMap(map);
         });
     }
 
+    /**
+     * Specify the z plane that should be visualized. This will hide all other
+     * layers that belong to this channel.
+     * @param {number} z - The new currently active z plane.
+     */
     setZplane(z: number) {
         console.log('channel: ', z);
         if (z == this._currentZplane) {
@@ -57,6 +98,10 @@ class Channel implements Layer {
         this._currentZplane = z;
     }
 
+    /**
+     * @property {Color} color - The color with which this channel is to be multiplied.
+     * @default Color.WHITE
+     */
     get color(): Color {
         return _.values(this._layers)[0].color;
     }
@@ -67,6 +112,11 @@ class Channel implements Layer {
         });
     }
 
+    /**
+     * @property {number} min - The lower bound used when stretching intensity
+     * according to the formula I' = (I - min) / (max - min) * 255.
+     * @default 0
+     */
     get min(): number {
         return _.values(this._layers)[0].min;
     }
@@ -77,6 +127,11 @@ class Channel implements Layer {
         });
     }
 
+    /**
+     * @property {number} min - The upper bound used when stretching intensity
+     * according to the formula I' = (I - min) / (max - min) * 255.
+     * @default 255
+     */
     get max(): number {
         return _.values(this._layers)[0].max;
     }
@@ -87,6 +142,10 @@ class Channel implements Layer {
         });
     }
 
+    /**
+     * @property {number} brightness - The brightness of the layers belonging to this channel.
+     * @default 50
+     */
     get brightness(): number {
         return _.values(this._layers)[0].brightness;
     }
@@ -97,6 +156,11 @@ class Channel implements Layer {
         });
     }
 
+    /**
+     * @property {number} opacity - The opacity of the layers belonging to this
+     * channel. Ranges from 0 to 1.
+     * @default 50
+     */
     get opacity(): number {
         return _.values(this._layers)[0].opacity;
     }
@@ -107,6 +171,10 @@ class Channel implements Layer {
         });
     }
 
+    /**
+     * @property {boolean} visible - If this channel should be visible.
+     * @default 50
+     */
     get visible(): boolean {
         return this._visible;
     }
@@ -119,10 +187,20 @@ class Channel implements Layer {
         this._visible = val;
     }
 
+    /**
+     * @property {number} maxZ - The maximum z plane to which this channel can
+     * be visualized. Setting the value above this value has to effect.
+     */
     get maxZ(): number {
         return Math.max.apply(this, _.keys(this._layers));
     }
 
+    /**
+     * @property {number} minZ - The minimum z plane to which this channel can
+     * be visualized. Setting the value below this value has to effect.
+     * Normally this value is set to 0.
+     * @default 50
+     */
     get minZ(): number {
         return Math.min.apply(this, _.keys(this._layers));
     }
