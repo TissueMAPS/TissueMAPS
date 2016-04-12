1a962f3d74b7eef964302d3f7a75b76d45ccdd1b
hafenr <robin.hafen@gmail.com>
2016-04-12 11:46:35 +0200

    Add red-white-blue color map

--- app/src/core/tool/result/HeatmapResult.ts
+++ app/src/core/tool/result/HeatmapResult.ts
@@ -1,8 +1,20 @@
 class HeatmapResult extends LabelResult {
+    /**
+     * Return a function that maps a label that was assigned to a map
+     * object to a color which can be used to colorize the
+     * object's polygon on the map.
+     */
     getLabelColorMapper() {
         return (label) => {
             var normLabel = (label - this.attributes.min) / this.attributes.max;
-            return new Color(0, 256 * normLabel, 0);
+            if (normLabel <= 0.5) {
+                normLabel *= 2;
+                var rescaledLabel = 255 * normLabel;
+                return new Color(255, rescaledLabel, rescaledLabel);
+            } else {
+                var rescaledLabel = 255 * normLabel;
+                return new Color(255 - rescaledLabel, 255 - rescaledLabel, 255);
+            }
         };
     }
 }
