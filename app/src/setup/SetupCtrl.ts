2e360bdd8ed60a0c8e442e04002f28709ff823a4
hafenr <robin.hafen@gmail.com>
2016-04-18 23:23:51 +0200

    Enable POSTing of workflow descriptions

--- app/src/setup/SetupCtrl.ts
+++ app/src/setup/SetupCtrl.ts
@@ -3,6 +3,10 @@ class SetupCtrl {
     currentStage;
     args: any = {};
 
+    private _workflowDescription: any;
+
+    static $inject = ['experiment', 'workflowDescription', '$state'];
+
     get inUploadFiles() {
         return this.currentStage.name === 'uploadfiles';
     }
@@ -36,7 +40,7 @@ class SetupCtrl {
             if (idx >= 0) {
                 var inLastStage = idx == stages.length - 1
                 if (inLastStage) {
-                    console.log('in last stage');
+                    this._submitWorkflow();
                 } else {
                     var newStage = stages[idx + 1];
                     this.currentStage = newStage;
@@ -50,20 +54,42 @@ class SetupCtrl {
         }
     }
 
-    static $inject = ['experiment', 'workflowDescription', '$state'];
+    private _submitWorkflow() {
+        var desc = $.extend(true, {}, this._workflowDescription);
+        desc.stages.forEach((stage) => {
+            stage.steps.forEach((step) => {
+                step.batch_args = this.args[stage.name][step.name].batch_args;
+                step.submission_args = this.args[stage.name][step.name].submission_args;
+                var extraArgs = this.args[stage.name][step.name].extra_args;
+                step.extra_args = _.isEmpty(extraArgs) ? null : extraArgs;
+            });
+        });
+        this.experiment.submitWorkflow(desc);
+    }
+
+    get submitButtonText() {
+        if (this.currentStage.name === 'image_analysis') {
+            return 'Submit';
+        } else {
+            return 'Next';
+        }
+    }
 
     constructor(public experiment: Experiment,
                 public workflowDescription: any,
                 private _$state) {
         // TODO: remove
         this.currentStage = workflowDescription.stages[0];
+        this._workflowDescription = $.extend(true, {}, workflowDescription);
+        console.log(workflowDescription);
         window['args'] = this.args;
         workflowDescription.stages.forEach((stage) => {
             this.args[stage.name] = {};
             stage.steps.forEach((step) => {
                 this.args[stage.name][step.name] = {
                     batch_args: {},
-                    submission_args: {}
+                    submission_args: {},
+                    extra_args: {}
                 };
                 step.batch_args.forEach((arg) => {
                     this.args[stage.name][step.name].batch_args[arg.name] = arg.default;
@@ -71,6 +97,11 @@ class SetupCtrl {
                 step.submission_args.forEach((arg) => {
                     this.args[stage.name][step.name].submission_args[arg.name] = arg.default;
                 });
+                if (step.extra_args) {
+                    step.extra_args.forEach((arg) => {
+                        this.args[stage.name][step.name].extra_args[arg.name] = arg.default;
+                    });
+                }
             });
         });
         switch(experiment.status) {
