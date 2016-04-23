e1277213f838e99a1942eb786b09c95cb0476036
hafenr <robin.hafen@gmail.com>
2016-04-23 18:39:44 +0200

    Improve stage checking

--- tmaps/experiment/upload.py
+++ tmaps/experiment/upload.py
@@ -1,6 +1,8 @@
 import os.path as p
 import json
 import datetime
+import importlib
+import re
 
 from flask import request
 from flask.ext.jwt import jwt_required
@@ -12,6 +14,7 @@ from tmlib.models import (
     MicroscopeMetadataFile
 )
 from tmlib.models.status import FileUploadStatus
+import tmlib.workflow.metaconfig
 
 from tmaps.extensions.redis import redis_store
 from tmaps.api import api
@@ -50,6 +53,8 @@ def register_upload(acquisition):
     # Delete any old files
     for f in acquisition.microscope_image_files:
         db.session.delete(f)
+    for f in acquisition.microscope_metadata_files:
+        db.session.delete(f)
     db.session.commit()
 
     # file_key = 'acquisition:%d:upload:files' % acquisition.id
@@ -57,18 +62,60 @@ def register_upload(acquisition):
 
     # redis_store.set(registered_flag_key, True)
 
-    filenames = [secure_filename(f) for f in data['files']]
-    file_objects = \
-        [MicroscopeImageFile(name=f, acquisition_id=acquisition.id)
-         for f in filenames]
+    microscope_type = acquisition.plate.experiment.microscope_type
+    metaconfig = importlib.import_module(
+        'tmlib.workflow.metaconfig.%s' % microscope_type)
+    imgfile_regex = re.compile(metaconfig.IMAGE_FILE_REGEX_PATTERN)
+    metadata_regex = re.compile(metaconfig.METADATA_FILE_REGEX_PATTERN)
 
-    db.session.add_all(file_objects)
+    imgfiles = [secure_filename(f) for f in data['files'] if imgfile_regex.match(f)]
+    metafiles = [secure_filename(f) for f in data['files'] if metadata_regex.match(f)]
+
+    imgfile_objects = \
+        [MicroscopeImageFile(name=f, acquisition_id=acquisition.id)
+         for f in imgfiles]
+    metafile_objects = \
+        [MicroscopeMetadataFile(name=f, acquisition_id=acquisition.id)
+         for f in metafiles]
+
+    db.session.add_all(imgfile_objects + metafile_objects)
     db.session.commit()
 
     # redis_store.delete(file_key)  # clear before adding
     # redis_store.sadd(file_key, *filenames)
 
     return jsonify(message='Upload registered')
+
+
+@api.route('/acquisitions/<acquisition_id>/file-validity-check', methods=['POST'])
+@jwt_required()
+@get(Acquisition, check_ownership=True)
+def file_validity_check(acquisition):
+    data = json.loads(request.data)
+    if not 'files' in data:
+        raise MalformedRequestError()
+    if not type(data['files']) is list:
+        raise MalformedRequestError()
+
+    microscope_type = acquisition.plate.experiment.microscope_type
+    metaconfig = importlib.import_module(
+        'tmlib.workflow.metaconfig.%s' % microscope_type)
+
+    imgfile_regex = re.compile(metaconfig.IMAGE_FILE_REGEX_PATTERN)
+    metadata_regex = re.compile(metaconfig.METADATA_FILE_REGEX_PATTERN)
+
+    def check_file(fname):
+        is_imgfile = imgfile_regex.match(fname) is not None
+        is_metadata_file = metadata_regex.match(fname) is not None
+        return is_metadata_file or is_imgfile
+
+    data = json.loads(request.data)
+    filenames = [f['name'] for f in data['files']]
+    is_valid = map(check_file, filenames)
+
+    return jsonify({
+        'is_valid': is_valid
+    })
 
 
 @api.route('/acquisitions/<acquisition_id>/upload-file', methods=['POST'])
@@ -98,13 +145,26 @@ def upload_file(acquisition):
     if not f:
         raise MalformedRequestError('Missing file entry in this request.')
 
-    filename = secure_filename(f.filename)
-    file_objs = [fl for fl in acquisition.microscope_image_files if fl.name == filename]
+    # filename = secure_filename(f.filename)
+    filename = f.filename
+    imgfile_objs = [fl for fl in acquisition.microscope_image_files if fl.name == filename]
+    metafile_objs = [fl for fl in acquisition.microscope_metadata_files if fl.name == filename]
 
-    if len(file_objs) != 1:
-        raise ResourceNotFoundError(
-            'This file was not registered and therefrore won\'t be saved.')
-    file_obj = file_objs[0]
+    if len(imgfile_objs) > 1:
+        raise MalformedRequestError('Multiple image files found with this name.')
+    if len(metafile_objs) > 1:
+        raise MalformedRequestError('Multiple metadata files found with this name.')
+
+    is_imgfile = len(imgfile_objs) == 1
+    is_metafile = len(metafile_objs) == 1
+    if is_imgfile and is_metafile:
+        raise MalformedRequestError('This file was registered as a metadata and as an image file.')
+    if is_imgfile:
+        file_obj = imgfile_objs[0]
+    elif is_metafile:
+        file_obj = metafile_objs[0]
+    else:
+        raise MalformedRequestError('This file was registered never registered.')
 
     f.save(file_obj.location)
     file_obj.upload_status = FileUploadStatus.COMPLETE
