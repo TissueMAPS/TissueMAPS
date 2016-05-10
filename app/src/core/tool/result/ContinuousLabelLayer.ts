743cd2e649b830ff44629443d98c6dddbeea2174
HACKERMD <markusdherrmann@gmail.com>
2016-05-10 13:05:10 +0200

    Fix bug of rescaling values for continous label layer

--- app/src/core/tool/result/ContinuousLabelLayer.ts
+++ app/src/core/tool/result/ContinuousLabelLayer.ts
@@ -1,7 +1,7 @@
 class ContinuousLabelLayer extends LabelLayer {
     getLabelColorMapper() {
         return (label) => {
-            var normLabel = (label - this.attributes.min) / this.attributes.max;
+            var normLabel = (label - this.attributes.min) / (this.attributes.max - this.attributes.min);
             var rescaledLabel = 255 * normLabel;
             return new Color(255, 255 - rescaledLabel, 255 - rescaledLabel);
         };
