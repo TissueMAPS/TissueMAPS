325a1e3f209ef10efbb3fe15ce7eed721bbbef3c
HACKERMD <markusdherrmann@gmail.com>
2016-05-14 23:52:09 +0200

    Further develop Spark integration

---
+++ app/src/tools/SVM/SVMCtrl.ts
@@ -0,0 +1,39 @@
+interface SVMScope extends ToolWindowContentScope {
+    svm: SVMToolCtrl;
+    featureWidget: FeatureSelectionWidgetCtrl;
+    mapobjectTypeWidget: MapobjectTypeWidgetCtrl;
+    classSelectionWidget: ClassSelectionWidgetCtrl;
+}
+
+class SVMToolCtrl extends ToolCtrl {
+    static $inject = ['$scope', 'viewer'];
+
+    kernelOptions = ['rbf', 'linear'];
+    kernel = 'rbf';
+
+    constructor(public $scope: SVMScope,
+                public viewer: Viewer) {
+        super();
+    }
+
+    doClassify() {
+        // Build the request object
+        var selectedFeatures = this.$scope.featureWidget.selectedFeatures;
+
+        var trainingClasses = [];
+        this.$scope.classSelectionWidget.classes.forEach((cls) => {
+            trainingClasses.push({
+                name: cls.name,
+                object_ids: cls.mapobjectIds,
+                color: cls.color.toHex()
+            });
+        });
+
+        this.sendRequest({
+            chosen_object_type: this.$scope.mapobjectTypeWidget.selectedType,
+            selected_features: selectedFeatures,
+            kernel: 'rbf',
+            training_classes: trainingClasses
+        });
+    }
+}
