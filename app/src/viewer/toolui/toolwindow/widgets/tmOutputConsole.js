63ebf2e95184c92815f3e7fe2ae0d8bd48e24803
hafenr <robin.hafen@gmail.com>
2016-05-18 16:52:05 +0200

    Delete old stuff

--- app/src/viewer/toolui/toolwindow/widgets/tmOutputConsole.js
+++ app/src/viewer/toolui/toolwindow/widgets/tmOutputConsole.js
@@ -1,43 +1,43 @@
-angular.module('tmaps.toolwindow')
-.directive('tmOutputConsole', ['toolInstance', '$sce', function(toolInstance, $sce) {
+// angular.module('tmaps.toolwindow')
+// .directive('tmOutputConsole', ['toolInstance', '$sce', function(toolInstance, $sce) {
 
-    return {
-        restrict: 'E',
-        templateUrl: '/templates/tools/widgets/tm-output-console.html',
-        link: function(scope, element, attrs) {
-            toolInstance.socket
-            .$on('log', function(data) {
-                element.find('.messages').append(
-                    '<span class="entry">' +
-                        '<span class="sender">SERVER:</span>' +
-                        '<span class="message">' + data + '</span>' +
-                    '</span><br>'
-                );
-            });
+//     return {
+//         restrict: 'E',
+//         templateUrl: '/templates/tools/widgets/tm-output-console.html',
+//         link: function(scope, element, attrs) {
+//             toolInstance.socket
+//             .$on('log', function(data) {
+//                 element.find('.messages').append(
+//                     '<span class="entry">' +
+//                         '<span class="sender">SERVER:</span>' +
+//                         '<span class="message">' + data + '</span>' +
+//                     '</span><br>'
+//                 );
+//             });
 
-            scope.$watch(
-                function() {
-                    return element.find('.entry').length;
-                },
-                function(newVal, oldVal) {
-                    var msgContainer = element.find('.messages');
-                    msgContainer.animate({
-                        scrollTop: msgContainer.prop('scrollHeight')
-                    }, 100);
-                }
-            );
-        },
-        controller: ['$scope', function($scope) {
-                // var messages = [];
-            // toolInstance.socket
-            // .$on('log', function(data) {
-            //     essages.push(data);
-            // });
+//             scope.$watch(
+//                 function() {
+//                     return element.find('.entry').length;
+//                 },
+//                 function(newVal, oldVal) {
+//                     var msgContainer = element.find('.messages');
+//                     msgContainer.animate({
+//                         scrollTop: msgContainer.prop('scrollHeight')
+//                     }, 100);
+//                 }
+//             );
+//         },
+//         controller: ['$scope', function($scope) {
+//                 // var messages = [];
+//             // toolInstance.socket
+//             // .$on('log', function(data) {
+//             //     essages.push(data);
+//             // });
 
-            // $scope.getMessages = function() {
-            //     return $sce.trustAsHtml(messages.join('<br>'));
-            // };
-        }]
-    };
+//             // $scope.getMessages = function() {
+//             //     return $sce.trustAsHtml(messages.join('<br>'));
+//             // };
+//         }]
+//     };
 
-}]);
+// }]);
