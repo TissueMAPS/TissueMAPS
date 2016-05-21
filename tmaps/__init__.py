8485f3eca139d6f5c264e0cb3d45632c86cd22e5
HACKERMD <markusdherrmann@gmail.com>
2016-05-21 16:56:28 +0200

    Add logger for tools

--- tmaps/__init__.py
+++ tmaps/__init__.py
@@ -9,21 +9,24 @@ import mapobject
 import tool
 import serialize
 
+def configure_logging(name):
+    fmt = '%(asctime)s | %(levelname)-8s | %(name)-40s | %(message)s'
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter(fmt=fmt, datefmt=datefmt)
 
-fmt = '%(asctime)s | %(levelname)-8s | %(name)-40s | %(message)s'
-datefmt = '%Y-%m-%d %H:%M:%S'
-formatter = logging.Formatter(fmt=fmt, datefmt=datefmt)
+    log = logging.getLogger(name)
+    log.setLevel(logging.INFO)
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+    stderr_handler = logging.StreamHandler(stream=sys.stderr)
+    stderr_handler.name = 'err'
+    stderr_handler.setFormatter(formatter)
+    stderr_handler.setLevel(logging.WARN)
+    log.addHandler(stderr_handler)
 
-stderr_handler = logging.StreamHandler(stream=sys.stderr)
-stderr_handler.name = 'err'
-stderr_handler.setFormatter(formatter)
-stderr_handler.setLevel(logging.WARN)
-log.addHandler(stderr_handler)
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    stdout_handler.name = 'out'
+    stdout_handler.setFormatter(formatter)
+    log.addHandler(stdout_handler)
 
-stdout_handler = logging.StreamHandler(stream=sys.stdout)
-stdout_handler.name = 'out'
-stdout_handler.setFormatter(formatter)
-log.addHandler(stdout_handler)
+
+log = configure_logging(__name__)
