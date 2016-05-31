b6ebb7936e19389ee132b6f0bfbeb1ba7441f95a
HACKERMD <markusdherrmann@gmail.com>
2016-05-31 12:52:50 +0200

    Fix import of tmaps extensions

--- tmaps/extensions/__init__.py
+++ tmaps/extensions/__init__.py
@@ -1,6 +1,3 @@
-from .gc3pie import GC3PieEngine
-gc3pie_engine = GC3PieEngine()
-
 from flask.ext.redis import FlaskRedis
 redis_store = FlaskRedis()
 
@@ -12,8 +9,8 @@ from auth import jwt
 from spark import Spark
 spark = Spark()
 
-from .gc3pie import GC3PieEngine
-gc3pie_engine = GC3PieEngine()
+from gc3pie import GC3Pie
+gc3pie = GC3Pie()
 
 from flask.ext.uwsgi_websocket import GeventWebSocket
 websocket = GeventWebSocket()
