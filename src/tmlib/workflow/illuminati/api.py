2086addcae76660f5059ed9d15b0ee1915b4a5ae
HACKERMD <markusdherrmann@gmail.com>
2016-06-29 14:30:50 +0200

    Write more log output in illuminati step at level INFO

--- src/tmlib/workflow/illuminati/api.py
+++ src/tmlib/workflow/illuminati/api.py
@@ -484,7 +484,7 @@ class PyramidBuilder(ClusterRoutines):
                         column=t['column'], level=batch['level'],
                         channel_layer_id=layer.id
                     )
-                    logger.debug('creating tile: %s', tile_file.name)
+                    logger.info('creating tile: %s', tile_file.name)
                     tile = layer.extract_tile_from_image(
                         image_store[file.name], t['y_offset'], t['x_offset']
                     )
