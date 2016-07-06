66f378d8519285367abf587684276880ed25c104
HACKERMD <markusdherrmann@gmail.com>
2016-07-06 13:34:28 +0200

    Improve tests for modules

--- src/python/tests/test_modules.py
+++ src/python/tests/test_modules.py
@@ -4,30 +4,33 @@ import inspect
 import importlib
 
 
-def check_module_structure(module):
+def check_module_structure(name):
+    module = importlib.import_module('jtmodules.%s' % name)
     functions = inspect.getmembers(module, predicate=inspect.isfunction)
     function_names = [f[0] for f in functions]
     assert 'main' in function_names, (
-        'Module "%s" must implement a function called "main"' % module.__name__
+        'Module "%s" must implement a "main" function' % name
     )
     # TODO: VERSION
 
 
-def check_existence_of_docs(module):
+def check_existence_of_docs(name):
+    module = importlib.import_module('jtmodules.%s' % name)
     assert module.__doc__ is not None, (
-        'Documentation for module "%s" missing' % module.__name__
+        'Documentation for module "%s" missing' % name
     )
     functions = inspect.getmembers(module, predicate=inspect.isfunction)
     main_func = [f[1] for f in functions if f[0] == 'main'][0]
     assert main_func.__doc__ is not None, (
         'Documentation for "main" function in module "%s" missing'
-        % module.__name__
+        % name
     )
 
 
-def check_module_parameters(module, handles_filename):
+def check_module_parameters(name, handles_filename):
+    module = importlib.import_module('jtmodules.%s' % name)
     functions = inspect.getmembers(module, predicate=inspect.isfunction)
-    handles_filename = handles[module.__name__]
+    handles_filename = handles[name]
     with open(handles_filename, 'r') as f:
         handles_description = yaml.load(f)
     input_handles_names = [h['name'] for h in handles_description['input']]
@@ -38,13 +41,13 @@ def check_module_parameters(module, handles_filename):
                 assert arg in input_handles_names, (
                     'Argument "%s" of function "main" in module "%s" '
                     'is not described in corresponding handle file "%s"'
-                    % (arg, module.__name__, handles_filename)
+                    % (arg, name, handles_filename)
                 )
             for handle_name in input_handles_names:
                 assert handle_name in parameters.args, (
                     'Input handle "%s" described in handle file "%s" '
                     'is not an argument of function "main" of module "%s"'
-                    % (handle_name, handles_filename, module.__name__)
+                    % (handle_name, handles_filename, name)
                 )
 
 
@@ -53,7 +56,6 @@ def test_modules_content(modules):
         if not filename.endswith('.py'):
             continue
         print 'test module "%s"' % name
-        module = importlib.import_module('jtmodules.%s' % name)
-        check_module_structure(module)
-        check_existence_of_docs(module)
-        check_module_parameters(module)
+        check_module_structure(name)
+        check_existence_of_docs(name)
+        check_module_parameters(name)
