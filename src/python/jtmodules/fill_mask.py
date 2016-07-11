2fc497b45f405b4ebabba27c8c7bda8bdda25aac
HACKERMD <markusdherrmann@gmail.com>
2016-07-11 17:36:33 +0200

    Use mahotas library for filling holes

--- src/python/jtmodules/fill_mask.py
+++ src/python/jtmodules/fill_mask.py
@@ -1,4 +1,4 @@
-from scipy import ndimage as ndi
+import mahotas as mh
 
 VERSION = '0.0.1'
 
@@ -17,21 +17,20 @@ def main(mask, plot=False):
     -------
     Dict[str, numpy.ndarray[numpy.int32] or str]
         "filled_mask": filled binary image
-        "figure": html string in case `plot` is ``True``
+        "figure": JSON string figure representation
     '''
-    filled_mask = ndi.binary_fill_holes(mask)
+    filled_mask = mh.close_holes(mask)
 
     output = {'filled_mask': filled_mask}
     if plot:
         from jtlib import plotting
-
         plots = [
             plotting.create_mask_image_plot(mask, 'ul'),
             plotting.create_mask_image_plot(filled_mask, 'ur'),
         ]
-
         output['figure'] = plotting.create_figure(
-                                plots, title='Holes in mask filled.')
+            plots, title='Holes in mask filled.'
+        )
     else:
         output['figure'] = str()
 
