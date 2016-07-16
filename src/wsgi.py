d26cbbadfae5a0e6488d5414f740b0925d2152cf
HACKERMD <markusdherrmann@gmail.com>
2016-07-16 13:40:35 +0200

    Update server start script

--- src/wsgi.py
+++ src/wsgi.py
@@ -32,11 +32,22 @@ logo = """
 
 print logo
 
-configure_logging(logging.INFO)
-tmlib_logger = logging.getLogger('tmlib')
-tmlib_logger.setLevel(logging.INFO)
-tmserver_logger = logging.getLogger('tmserver')
-tmserver_logger.setLevel(logging.INFO)
+configure_logging()
+
+def set_logging_level(level):
+    logging_levels = {
+        'INFO': logging.INFO,
+        'DEBUG': logging.DEBUG,
+        'CRITICAL': logging.CRITICAL
+    }
+    if level not in logging_levels:
+        raise ValueError('Unknow logging level "%s".' % level)
+    tmlib_logger = logging.getLogger('tmlib')
+    tmlib_logger.setLevel(logging_levels[level])
+    tmserver_logger = logging.getLogger('tmserver')
+    tmserver_logger.setLevel(logging_levels[level])
+    wsgi_logger = logging.getLogger('wsgi')
+    wsgi_logger.setLevel(logging_levels[level])
 
 app = create_app()
 
@@ -44,18 +55,25 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='TissueMAPS server')
     parser.add_argument(
-        '--port', action='store', type=int, default=5002,
-        help='the port on which the server should listen')
-    # parser.add_argument(
-    #     '--threaded', action='store_true', default=False,
-    #     help='if the dev server should run in multi-threaded mode')
+        '--port', type=int, default=5002,
+        help='the port on which the server should listen (default: 5002)'
+    )
     parser.add_argument(
         '--gevent', action='store_true', default=False,
-        help='if the dev server should run in gevent mode')
+        help='if the dev server should run in gevent mode'
+    )
+    parser.add_argument(
+        '--logging', dest='logging_level', type=str, default='INFO',
+        choices={'CRITICAL', 'INFO', 'DEBUG'},
+        help='logging level (default: INFO)'
+    )
     parser.add_argument(
         '--profile', action='store_true', default=False,
-        help='if application should be profiled')
+        help='if application should be profiled'
+    )
     args = parser.parse_args()
+
+    set_logging_level(args.logging_level)
 
     if args.profile:
         app.config['PROFILE'] = True
