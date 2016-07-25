67a230dd5673601f2e1f1a8c3deb8597f29287db
HACKERMD <markusdherrmann@gmail.com>
2016-07-25 10:57:53 +0200

    Increase default batch size for align step

--- src/tmlib/workflow/align/args.py
+++ src/tmlib/workflow/align/args.py
@@ -21,7 +21,7 @@ class AlignBatchArguments(BatchArguments):
     )
 
     batch_size = Argument(
-        type=int, default=10, flag='b',
+        type=int, default=100, flag='b',
         help='number of image files that should be processed per job'
     )
 
