e28b666a86c79e4487a98769cf9ff93e4119ffc7
HACKERMD <markusdherrmann@gmail.com>
2016-08-02 01:11:55 +0200

    Provide dynamic list of supported microscope types and acquisition modes

--- src/app/src/userpanel/CreateExperimentCtrl.ts
+++ src/app/src/userpanel/CreateExperimentCtrl.ts
@@ -1,6 +1,8 @@
 class CreateExperimentCtrl {
 
     error: string;
+    supportedMicroscopeTypes: any;
+    supportedAcquisitionModes: any;
 
     opt = {
         name: undefined,
@@ -10,9 +12,42 @@ class CreateExperimentCtrl {
         plateAcquisitionMode: 'basic'
     };
 
+    private _$http: ng.IHttpService;
+    private _$q: ng.IQService;
     static $inject = ['$scope', '$state'];
 
-    constructor(private _$scope, private _$state) {}
+    constructor(private _$scope, private _$state) {
+        this._$http = $injector.get<ng.IHttpService>('$http');
+        this._$q = $injector.get<ng.IQService>('$q');
+        this._getMicroscopeTypes();
+        this._getAcquisitionModes();
+    }
+
+    private _getMicroscopeTypes(): ng.IPromise<any> {
+        return this._$http.get('/api/microscope_types')
+        .then((resp: any) => {
+            // console.log(resp)
+            // experiment.workflowDescription = resp.data.data;
+            this.supportedMicroscopeTypes = resp.data.data;
+            return resp.data.data;
+        })
+        .catch((resp) => {
+            return this._$q.reject(resp.data.error);
+        });
+    }
+
+    private _getAcquisitionModes(): ng.IPromise<any> {
+        return this._$http.get('/api/acquisition_modes')
+        .then((resp: any) => {
+            // console.log(resp)
+            // experiment.workflowDescription = resp.data.data;
+            this.supportedAcquisitionModes = resp.data.data;
+            return resp.data.data;
+        })
+        .catch((resp) => {
+            return this._$q.reject(resp.data.error);
+        });
+    }
 
     createExperiment() {
         var opt = this.opt;
