861428f8e573b7440456a6a7910c1d019e2c4138
HACKERMD <markusdherrmann@gmail.com>
2016-08-09 02:29:14 +0200

    Fix bug: gaussian filter should return integer array

--- src/python/jtmodules/smooth.py
+++ src/python/jtmodules/smooth.py
@@ -2,7 +2,6 @@ import cv2
 import mahotas as mh
 import skimage.morphology
 import skimage.filters.rank
-import skimage.measure
 import numpy as np
 
 VERSION = '0.0.1'
@@ -10,14 +9,10 @@ VERSION = '0.0.1'
 
 def main(image, filter_name, filter_size, sigma=0, sigma_color=0,
                  sigma_space=0, plot=False):
-    '''Smoothes an image.
+    '''Smoothes (blurs) an image using a low-pass filter.
 
-    For more information on "average", "gaussian" and "bilateral" filters see
+    For more information on image filtering see
     `OpenCV tutorial <http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_smoothed_imageproc/py_filtering/py_filtering.html>`_.
-
-    For more information on "median" filter_name see
-    `scikit-image docs <http://scikit-image.org/docs/dev/api/skimage.filters.html#median>`_.
-
 
     Parameters
     ----------
@@ -27,13 +22,13 @@ def main(image, filter_name, filter_size, sigma=0, sigma_color=0,
         name of the filter kernel that should be applied
         (options: ``{"avarage", "gaussian", "median", "median-bilateral", "gaussian-bilateral"}``)
     filter_size: int
-        size (width/height) of the kernel (must be positive and odd)
+        size (width/height) of the kernel (must be an odd, positive integer)
     sigma_color: int, optional
-        Gaussian component (filter_name sigma) applied in the intensity domain
-        (color space) - only relevant for "bilateral" filter_name (default: ``0``)
+        Gaussian component (sigma) applied in the intensity domain
+        (color space) - only relevant for "bilateral" filter (default: ``0``)
     sigma_space: int, optional
-        Gaussian component (filter_name sigma) applied in the spacial domain
-        (coordinate space) - only relevant for "bilateral" filter_name
+        Gaussian component (sigma) applied in the spacial domain
+        (coordinate space) - only relevant for "bilateral" filter
         (default: ``0``)
     plot: bool, optional
         whether a plot should be generated (default: ``False``)
@@ -48,56 +43,58 @@ def main(image, filter_name, filter_size, sigma=0, sigma_color=0,
     ------
     ValueError
         when `filter_name` is not
-        ``"avarage"``, ``"gaussian"``, ``"median"``, or ``"bilateral"``
-    TypeError
-        when `image` does not have unsigned integer type
+        ``"avarage"``, ``"gaussian"``, ``"median"``, ``"gaussian-bilateral"``
+        or ``"gaussian-bilateral"
     '''
-    input_dtype = image.dtype
-    if not str(image.dtype).startswith('uint'):
-        raise TypeError('Image must have unsigned integer type.')
-
     if filter_name == 'average':
+        logger.info('apply "average" filter')
         smoothed_image = cv2.blur(
             image, (filter_size, filter_size)
         )
     elif filter_name == 'gaussian':
+        logger.info('apply "gaussian" filter')
         smoothed_image = mh.gaussian_filter(
             image, sigma=filter_size
-        )
+        ).astype(image.dtype)
     elif filter_name == 'gaussian-bilateral':
+        logger.info('apply "gaussian-bilateral" filter')
         smoothed_image = cv2.bilateralFilter(
             image, filter_size, sigma_color, sigma_space
         )
     elif filter_name == 'median':
+        logger.info('apply "median" filter')
         smoothed_image = mh.median_filter(
             image, np.ones((filter_size, filter_size), dtype=image.dtype)
         )
     elif filter_name == 'median-bilateral':
+        logger.info('apply "median-bilateral" filter')
         smoothed_image = skimage.filters.rank.mean_bilateral(
             image, skimage.morphology.disk(filter_size),
             s0=sigma_space, s1=sigma_space
         )
     else:
         raise ValueError(
-            'Unknown filter_name. Implemented filters are:\n'
-            '"average", "gaussian", "median", and "bilateral"'
+            'Arugment "filter_name" can be one of the following:\n'
+            '"average", "gaussian", "median", and "gaussian-bilateral" and '
+            '"median-bilateral"'
         )
 
-    output = {'smoothed_image': smoothed_image.astype(input_dtype)}
+    output = {'smoothed_image': smoothed_image.astype(image.dtype)}
     if plot:
+        logger.info('create plot')
         from jtlib import plotting
-
         clip_value = np.percentile(image, 99.99)
         data = [
             plotting.create_intensity_image_plot(
-                        image, 'ul', clip_value=clip_value),
+                image, 'ul', clip_value=clip_value
+            ),
             plotting.create_intensity_image_plot(
-                        smoothed_image, 'ur', clip_value=clip_value),
+                smoothed_image, 'ur', clip_value=clip_value
+            ),
         ]
-
         output['figure'] = plotting.create_figure(
             data,
-            title='image smoothed with {0} filter (kernel size: {1})'.format(
+            title='smoothed with {0} filter (kernel size: {1})'.format(
                 filter_name, filter_size
             )
         )
