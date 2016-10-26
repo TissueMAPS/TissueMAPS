57997c4ab3a725cce3c325dafee804d17e596656
HACKERMD <markusdherrmann@gmail.com>
2016-10-26 15:31:34 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setuptools.setup(
         'cython>=0.24',
         'scikit-image>=0.11.3',
         'mahotas>=1.4.0',
-        'centrosome>=1.0.3',
+        'centrosome>=1.0.5',
         'pytest>=2.8.2',
         'tox>=2.3.1'
     ]
