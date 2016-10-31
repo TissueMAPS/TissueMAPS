0acb7a3d2ce176b31f68160c5981445ccb8adf77
HACKERMD <markusdherrmann@gmail.com>
2016-11-01 00:58:09 +0100

    Add Apache license

--- src/app/src/core/Viewer.ts
+++ src/app/src/core/Viewer.ts
@@ -1,3 +1,16 @@
+// Copyright 2016 Markus D. Herrmann, University of Zurich and Robin Hafen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 // interface SerializedViewer extends Serialized<Viewer> {
 //     experiment: SerializedExperiment;
 //     viewport: SerializedViewport;
@@ -212,7 +225,7 @@ class Viewer {
                 var submissionIds = this.savedResults.map((res) => {
                     return res.submissionId;
                 })
-                if (!submissionIds.includes(result.submissionId) && result.submissionId != this.currentResult.submissionId) {
+                if (submissionIds.indexOf(result.submissionId) == -1 && result.submissionId != this.currentResult.submissionId) {
                     console.log('save result: ', result)
                     this.savedResults.push(result);
                 }
