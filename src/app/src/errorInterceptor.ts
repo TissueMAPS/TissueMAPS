84ce152aec74a69fd40f068b3679358c79d3354d
hafenr <robin.hafen@gmail.com>
2016-10-31 16:00:44 +0100

    Modify error handler to display error message

--- src/app/src/errorInterceptor.ts
+++ src/app/src/errorInterceptor.ts
@@ -1,26 +1,34 @@
 angular.module('tmaps.ui')
 .factory('errorInterceptor', ['$injector', function($injector) {
 
-    function response(resp) {
+    function responseError(resp) {
         // Inject directly because of circular dependency hell
         var $uibModal = $injector.get('$uibModal');
         var $q = $injector.get('$q');
         // Check if the response was a flask-generated custom JSON
         // error response.
         if (resp.data.error) {
+            var error = resp.data.error;
             // The error payload should contain a message property that describes this error.
             var errorMessage =
-                resp.data.message || resp.data.type || ('Error ' + resp.data.status_code);
+                error.message || error.type || ('Error ' + error.status_code);
             var instance = $uibModal.open({
-                template: errorMessage
+                templateUrl: '/src/error.html',
+                resolve: {
+                    errorMessage: function() {
+                        return errorMessage;
+                    }
+                },
+                controller: ['$scope', 'errorMessage', function($scope, errorMessage) {
+                    $scope.errorMessage = errorMessage;
+                }]
             });
         }
-        return resp;
         return resp || $q.when(resp);
     }
 
     return {
-        response: response
+        responseError: responseError
     };
 
 }]);
