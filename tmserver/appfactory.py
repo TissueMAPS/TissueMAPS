fcaffd2589b33120239fac83ebb27bf7aca58213
HACKERMD <markusdherrmann@gmail.com>
2016-12-09 19:05:03 +0100

    Change logging level of gc3pie logger

--- tmserver/appfactory.py
+++ tmserver/appfactory.py
@@ -128,23 +128,18 @@ def create_app(config_overrides={}, verbosity=None):
     werkzeug_logger = logging.getLogger('werkzeug')
     werkzeug_logger.addHandler(file_handler)
     werkzeug_logger.addHandler(stdout_handler)
-    if verbosity > 5:
+    if verbosity > 4:
         gc3pie_logger.setLevel(logging.DEBUG)
         wsgi_logger.setLevel(logging.DEBUG)
         werkzeug_logger.setLevel(logging.DEBUG)
         apscheduler_logger.setLevel(logging.DEBUG)
-    elif verbosity > 4:
-        gc3pie_logger.setLevel(logging.INFO)
-        wsgi_logger.setLevel(logging.DEBUG)
-        werkzeug_logger.setLevel(logging.DEBUG)
-        apscheduler_logger.setLevel(logging.DEBUG)
     elif verbosity > 3:
-        gc3pie_logger.setLevel(logging.ERROR)
+        gc3pie_logger.setLevel(logging.INFO)
         wsgi_logger.setLevel(logging.INFO)
         werkzeug_logger.setLevel(logging.INFO)
         apscheduler_logger.setLevel(logging.INFO)
     else:
-        gc3pie_logger.setLevel(logging.CRITICAL)
+        gc3pie_logger.setLevel(logging.ERROR)
         wsgi_logger.setLevel(logging.ERROR)
         werkzeug_logger.setLevel(logging.ERROR)
         apscheduler_logger.setLevel(logging.ERROR)
