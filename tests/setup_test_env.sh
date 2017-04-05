a117e5048319ef2f4cf4568c41d16ca95ccfad2e
HACKERMD <markusdherrmann@gmail.com>
2017-04-05 13:49:56 +0200

    Include installation of Docker CE

--- tests/setup_test_env.sh
+++ tests/setup_test_env.sh
@@ -2,9 +2,32 @@
 
 # Install requirements
 echo "Install required apt packages..."
+sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
 sudo apt-get update
-sudo apt-get -y install python-dev build-essential libssl-dev python-apt python-pip python-setuptools wget vim
-sudo pip install --upgrade pip setuptools virtualenvwrapper pytest
+sudo apt-get -y install \
+    python-dev \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    python-apt \
+    python-pip \
+    python-setuptools \
+    wget \
+    curl \
+    apt-transport-https \
+    ca-certificates \
+    docker-ce
+
+echo "Install required pip packages..."
+sudo pip install --upgrade \
+    pip \
+    setuptools \
+    virtualenvwrapper \
+    pytest
+
 
 # Setup Python virtual environment
 echo "Create Python virtual environment..."
@@ -14,17 +37,19 @@ mkvirtualenv tmtest
 
 # Install TissueMAPS Python packages
 echo "Install tmdeploy package..."
-pip install https://github.com/tissuemaps/tmdeploy/tarball/master#egg=tmdeploy
+pip install tmdeploy
+
 echo "Install tmclient package..."
-pip install https://github.com/tissuemaps/tmclient/tarball/master#egg=tmclient
+pip install tmclient
 
 # Build Docker containers and start them in the background
 echo "Build Docker containers..."
 tm_deploy -v container build
+
 echo "Start Docker containers..."
 tm_deploy -v container start
-echo "=> You can connect to the TissueMAPS server on port 8002"
 
+echo "=> You can connect to the TissueMAPS server on localhost port 8002"
 exit 0
 
 
