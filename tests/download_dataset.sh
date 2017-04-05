29b489794b34683768133eefc3b587e233e7fbd9
HACKERMD <markusdherrmann@gmail.com>
2017-04-05 14:46:21 +0200

    Rename variables in script for downloading dataset

---
+++ tests/download_dataset.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+EXPERIMENT_NAME=$1
+
+DATA_DIR=$HOME/tmtestdata
+USERNAME=storage
+PASSWORD=o62ReU98h9Yb
+
+echo "Download test dataset '$EXPERIMENT_NAME'..."
+wget --user $USERNAME --password $PASSWORD -p -r -nH -np -R index.html* http://testdata.tissuemaps.org/storage/$EXPERIMENT_NAME -P $DATA_DIR
+
+export TMTEST_DATASET_DIR="$DATA_DIR/storage/$EXPERIMENT_NAME"
+
+exit 0
