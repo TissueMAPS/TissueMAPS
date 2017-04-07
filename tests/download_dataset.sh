94824748d06d838e9dce2ee8a8844c94c8f368f3
jluethi <jl.twenty-four@hotmail.com>
2017-04-07 14:23:11 +0200

    Update path to fileserver with testdata

--- tests/download_dataset.sh
+++ tests/download_dataset.sh
@@ -7,7 +7,7 @@ USERNAME=storage
 PASSWORD=o62ReU98h9Yb
 
 echo "Download test dataset '$EXPERIMENT_NAME'..."
-wget --user $USERNAME --password $PASSWORD -p -r -nH -np -R index.html* http://testdata.tissuemaps.org/storage/$EXPERIMENT_NAME -P $DATA_DIR
+wget --user $USERNAME --password $PASSWORD -p -r -nH -np -R index.html* http://data.tissuemaps.org/storage/$EXPERIMENT_NAME -P $DATA_DIR
 
 export TMTEST_DATASET_DIR="$DATA_DIR/storage/$EXPERIMENT_NAME"
 
