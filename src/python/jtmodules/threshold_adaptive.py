446e43578eed1f85f58cf3e5157c6b563f8b27c0
HACKERMD <markusdherrmann@gmail.com>
2017-04-16 17:36:16 +0200

    Add niblack method to threshold_adaptive module

--- src/python/jtmodules/threshold_adaptive.py
+++ src/python/jtmodules/threshold_adaptive.py
@@ -22,28 +22,39 @@ import collections
 import mahotas as mh
 import cv2
 import numpy as np
+from jtlib.utils import rescale_to_8bit
+from scipy.ndimage import generic_filter
 
 logger = logging.getLogger(__name__)
 
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 
 Output = collections.namedtuple('Output', ['mask', 'figure'])
 
 
-def main(image, kernel_size, offset=0, plot=False):
-    '''Thresholds an image with a locally adaptive threshold, where locality
-    is defined by `kernel_size`.
+def main(image, method, kernel_size, constant=0,
+        min_threshold=None, max_threshold=None, plot=False):
+    '''Thresholds an image with a locally adaptive threshold method.
 
     Parameters
     ----------
     image: numpy.ndarray
         grayscale image that should be thresholded
+    method: str
+        thresholding method (options: ``{"crosscorr", "niblack"}``)
     kernel_size: int
         size of the neighbourhood region that's used to calculate the threshold
         value at each pixel position (must be an odd number)
-    offset: int, optional
-        offset that is added to the mean calculated per neighbourhood region
-        (may be positive or negative)
+    constant: Union[float, int], optional
+        depends on `method`; in case of ``"crossvar"`` method the value
+        is subtracted from the computed weighted sum per neighbourhood region
+        and in case of ``"niblack"`` the value is multiplied by the
+        standard deviation and this term is then subtracted from the mean
+        computed per neighbourhood region
+    min_threshold: int, optional
+        minimal threshold level (default: ``numpy.min(image)``)
+    max_threshold: int, optional
+        maximal threshold level (default: ``numpy.max(image)``)
     plot: bool, optional
         whether a plot should be generated (default: ``False``)
 
@@ -54,23 +65,55 @@ def main(image, kernel_size, offset=0, plot=False):
     Raises
     ------
     ValueError
-        when `kernel_size` is not an odd number
+        when `kernel_size` is not an odd number or when `method` is not valid
+
+    Note
+    ----
+    Typically requires prior filtering to reduce noise in the image.
+
+    References
+    ----------
+    .. [1] Niblack, W. 1986: An introduction to Digital Image Processing, Prentice-Hall.
     '''
     if kernel_size % 2 == 0:
         raise ValueError('Argument "kernel_size" must be an odd integer.')
     logger.debug('set kernel size: %d', kernel_size)
 
+    if max_threshold is None:
+        max_threshold = np.max(image)
+    logger.debug('set maximal threshold: %d', max_threshold)
+
+    if min_threshold is None:
+        min_threshold = np.min(image)
+    logger.debug('set minimal threshold: %d', min_threshold)
+
     logger.debug('map image intensities to 8-bit range')
-    image_8bit = mh.stretch(image)
+    image_8bit = rescale_to_8bit(image, upper=99.99)
 
     logger.info('threshold image')
-    thresh_image = cv2.adaptiveThreshold(
-        image_8bit, 255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
-        kernel_size, offset
-    )
+    if method == 'crosscorr':
+        thresh_image = cv2.adaptiveThresholding(
+            image_8bit, maxValue=255,
+            adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            type=cv2.THRESH_BINARY,
+            blockSize=kernel_size, offset=int(constant)
+        )
+    elif method == 'niblack':
+        thresh_image = cv2.ximgproc.niBlackThreshold(
+            image_8bit, maxValue=255, type=cv2.THRESH_BINARY,
+            blockSize=kernel_size, delta=constant
+        )
+    else:
+        raise ValueError(
+            'Arugment "method" can be one of the following:\n'
+            '"crosscorr" or "niblack"'
+        )
     # OpenCV treats masks as unsigned integer and not as boolean
     thresh_image = thresh_image > 0
+
+    # Manually fine tune automatic thresholding result
+    thresh_image[image < min_threshold] = False
+    thresh_image[image > max_threshold] = True
 
     if plot:
         logger.info('create plot')
