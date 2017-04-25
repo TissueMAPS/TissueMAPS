3c5de16b19ab790d0c3313383f72b44713864b3d
Scott Berry <scottdberry@gmail.com>
2017-04-25 15:24:48 +0000

    Added docstring and licence info to project.py

--- src/python/jtmodules/project.py
+++ src/python/jtmodules/project.py
@@ -1,3 +1,18 @@
+# Copyright 2016 Markus D. Herrmann, University of Zurich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''Jterator module for making a projection along the final dimension of the
+ array'''
 import numpy as np
 import collections
 import logging
@@ -12,7 +27,6 @@ projections = {
     'max': np.max,
     'sum': np.sum
 }
-
 
 def main(image, method='max', plot=False):
     '''Projects an image along the last dimension using the given `method`.
@@ -39,9 +53,8 @@ def main(image, method='max', plot=False):
                 projected_image, 'ul', clip=True
             )
 	]
-        figure = plotting.create_figure(plots, title='projected image')
+        figure = plotting.create_figure(plots, title='projection image')
     else:
         figure = str()
 
     return Output(projected_image, figure)
-
