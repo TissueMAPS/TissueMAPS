29b806810b741a618844c45968228b4485459e55
HACKERMD <markusdherrmann@gmail.com>
2017-05-16 16:02:02 +0200

    Update illuminati collect phase

--- tmlib/workflow/illuminati/api.py
+++ tmlib/workflow/illuminati/api.py
@@ -654,21 +654,12 @@ class PyramidBuilder(WorkflowStepAPI):
                 logger.debug('add new mapobjects of type "%s"', name)
                 for key, value in segmentations.iteritems():
                     mapobject = tm.Mapobject(mapobject_type_id, ref_id=key)
-<<<<<<< HEAD
                     mapobject = tm.Mapobject.add(conn, mapobject)
                     logger.debug('add mapobject #%d', mapobject.id)
                     mapobject_segmentation = tm.MapobjectSegmentation(
                         geom_polygon=value['polygon'],
                         geom_centroid=value['polygon'].centroid,
                         mapobject_id=mapobject.id,
-=======
-                    mapobject_id = tm.Mapobject.add(conn, mapobject)
-                    logger.debug('add mapobject #%d', mapobject_id)
-                    mapobject_segmentation = tm.MapobjectSegmentation(
-                        geom_polygon=value['polygon'],
-                        geom_centroid=value['polygon'].centroid,
-                        mapobject_id=mapobject_id,
->>>>>>> Enable single-shard copy for bulk data ingestion
                         segmentation_layer_id=value['segmentation_layer_id'],
                     )
                     tm.MapobjectSegmentation.add(conn, mapobject_segmentation)
