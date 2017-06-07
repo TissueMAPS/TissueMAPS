53fbdd10c132830d86433aeb3cdc477f354e928f
jluethi <jl.twenty-four@hotmail.com>
2017-06-07 13:08:13 +0200

    Increase version to 0.2.2 for release

--- tmserver/version.py
+++ tmserver/version.py
@@ -13,5 +13,5 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '0.2.2rc'
+__version__ = '0.2.2'
 
