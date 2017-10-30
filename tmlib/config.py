830d885752d941891d6fd04e41e4eb8edf64285e
Riccardo Murri <riccardo.murri@gmail.com>
2017-10-30 11:23:52 +0100

    Fix "NameError: global name 'Iterable' is not defined"

--- tmlib/config.py
+++ tmlib/config.py
@@ -13,6 +13,10 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+try:
+    from collections.abc import Iterable  # Python 3
+except ImportError:
+    from collections import Iterable  # Python 2.7
 import os
 from abc import ABCMeta
 import logging
