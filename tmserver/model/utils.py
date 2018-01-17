190d98aac371fa3d9224c729184fe05bc8eba653
Riccardo Murri <riccardo.murri@gmail.com>
2018-01-17 15:35:06 +0100

    Use simple decimal external representation for IDs.
    There is actually little reason to obfuscate database IDs for an
    open-source software where everyone can read what the obfuscation
    algorithm is.  Obfuscation just makes reading URLs and logs harder for
    no gain.
    
    Fixes TissueMAPS/TissueMAPS#90

--- tmserver/model/utils.py
+++ tmserver/model/utils.py
@@ -1,5 +1,6 @@
 # TmServer - TissueMAPS server application.
 # Copyright (C) 2016  Markus D. Herrmann, University of Zurich and Robin Hafen
+# Copyright (C) 2018  University of Zurich
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
@@ -18,13 +19,20 @@ import base64
 
 
 def decode_pk(pk_str):
-    """Decode a hashed database ID so that it can be used
-    when formulating SQL queries.
+    """
+    Decode a database ID from its representation in GET and POST requests.
+
+    The current external representation of database IDs is just the
+    printed representation of the ID as a decimal number, but this
+    function can decode also older encodings as base64-obfuscated
+    strings.
+
+    See :func:`encode_pk`.
 
     Parameters
     ----------
     pk_str : str
-        hashed database ID
+        external representation of database ID
 
     Returns
     ------
@@ -35,21 +43,28 @@ def decode_pk(pk_str):
     ------
     ValueError
         when the hashed ID can't be decoded.
-
     """
     try:
-        pk = int(base64.urlsafe_b64decode(str(pk_str))[5:])
+        return int(pk_str)
+    except (TypeError, ValueError):
+        pass  # assume we've got an old base64-encoded thing
+    # decode old base64-encoding
+    try:
+        return int(base64.urlsafe_b64decode(str(pk_str))[5:])
     except Exception as e:
         raise ValueError(
             'Cannot decode hashed primary key %s. '
-            'Original exception: %s' % (pk_str, str(e)))
-    else:
-        return pk
+            'Original exception: %s' % (pk_str, e))
 
 
 def encode_pk(pk):
-    """Encode a database id as a string such that it is not directly
-    visible to the client
+    """
+    Encode a database id as a string.
+
+    Currently the encoding is just the representation of `pk` as a
+    string of decimal digits.  (Former versions of this function used
+    to munge and base64-encode the ID to obfuscate it, but is no
+    apparent reason to do so.)
 
     Parameters
     ----------
@@ -60,6 +75,5 @@ def encode_pk(pk):
     ------
     str
         hashed database ID
-
     """
-    return base64.urlsafe_b64encode('tmaps' + str(pk))
+    return ('%d' % pk)
