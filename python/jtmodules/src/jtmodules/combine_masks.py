14db3a8083f877b437151af2020c2f68b7cf5575
Joel Lüthi <jl.twenty-four@hotmail.com>
2018-02-28 12:26:29 +0100

    Improved combine masks (#19)
    Improve the module so that it can:
    - handle both binary and label images
    - Do logical AND, logical OR & logical XOR comparisons

--- python/jtmodules/src/jtmodules/combine_masks.py
+++ python/jtmodules/src/jtmodules/combine_masks.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 '''Jterator module for combining objects from two binary mask images into one
-by performing an element-wise logical OR operation.
+by performing an element-wise logical operation [AND or OR or XOR].
 '''
 import numpy as np
 import logging
@@ -20,28 +20,44 @@ import collections
 
 logger = logging.getLogger(__name__)
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 Output = collections.namedtuple('Output', ['combined_mask', 'figure'])
 
 
-def main(mask_1, mask_2, plot=False):
+def main(mask_1, mask_2, logical_operation, plot=False):
     '''Combines two binary masks, such that the resulting combined mask
     is ``True`` where either `mask_1` OR `mask_2` is ``True``.
-
     Parameters
     ----------
-    mask_1: numpy.ndarray[numpy.bool]
-        2D binary array
-    mask_2: numpy.ndarray[numpy.bool]
-        2D binary array
-
+    mask_1: numpy.ndarray[Union[numpy.bool, numpy.int32]]
+        binary or labeled mask
+    mask_2: numpy.ndarray[Union[numpy.bool, numpy.int32]]
+        binary or labeled mask
+    logical_operation: str
+        name of the logical operation to be applied
+        (options: ``{"AND", "OR", "EXCLUSIVE_OR"}``)
     Returns
     -------
     jtmodules.combine_objects.Output
-
     '''
-    combined_mask = np.logical_or(mask_1, mask_2)
+    mask_1 = mask_1 != 0
+    mask_2 = mask_2 != 0
+    
+    if logical_operation == "AND":
+        logger.info('Apply logical AND')
+        combined_mask = np.logical_and(mask_1, mask_2)
+    elif logical_operation == "OR":
+        logger.info('Apply logical OR')
+        combined_mask = np.logical_or(mask_1, mask_2)
+    elif logical_operation == "EXCLUSIVE_OR":
+        logger.info('Apply logical XOR')
+        combined_mask = np.logical_xor(mask_1, mask_2)
+    else:
+            raise ValueError(
+                'Arugment "logical_operation" can be one of the following:\n'
+                '"AND", "OR", "EXCLUSIVE_OR"'
+            )
 
     if plot:
         from jtlib import plotting
@@ -55,4 +71,3 @@ def main(mask_1, mask_2, plot=False):
         figure = str()
 
     return Output(combined_mask, figure)
-
