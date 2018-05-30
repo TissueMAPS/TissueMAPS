e284c1747d0a067360888a8e9269c1ccb87744ad
Riccardo Murri <riccardo.murri@gmail.com>
2018-05-30 13:49:20 +0200

    Bump version to 0.4.0rc1

--- tmserver/version.py
+++ tmserver/version.py
@@ -13,5 +13,5 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '0.3.3'
+__version__ = '0.4.0rc1'
 
