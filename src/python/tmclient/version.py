6fb40622af19142cb5169a64b8c2965993a25ab1
Riccardo Murri <riccardo.murri@gmail.com>
2018-08-15 16:57:02 +0200

    Bump version to 0.4.1

--- src/python/tmclient/version.py
+++ src/python/tmclient/version.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Markus D. Herrmann, University of Zurich
+# Copyright 2016-2018 University of Zurich.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = '0.3.3'
+__version__ = '0.4.1'
